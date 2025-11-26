7553e523e43a0ed2a44c7733d580cab558d0f843
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-26 18:48:07 -0500

    Created the basic assets for the pause menu

--- Script/PokeSharpSample.Glue/GameplayTags.cs
+++ Script/PokeSharpSample.Glue/GameplayTags.cs
@@ -8,4 +8,8 @@ public static class GameplayTags
 	public static readonly FGameplayTag Input_Touch = new("Input.Touch");
 	public static readonly FGameplayTag InputMode_Game = new("InputMode.Game");
 	public static readonly FGameplayTag InputMode_Menu = new("InputMode.Menu");
+	public static readonly FGameplayTag UI_Layer_Game = new("UI.Layer.Game");
+	public static readonly FGameplayTag UI_Layer_GameMenu = new("UI.Layer.GameMenu");
+	public static readonly FGameplayTag UI_Layer_Menu = new("UI.Layer.Menu");
+	public static readonly FGameplayTag UI_Layer_Modal = new("UI.Layer.Modal");
 }
